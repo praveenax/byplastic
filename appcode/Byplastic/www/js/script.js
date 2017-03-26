22a7b75d620ce0bf7440aa1669ce543adb81be69
Praveen <praveenax@airloyal.com>
2017-03-26 09:31:33 +0530

    latest changes

--- appcode/Byplastic/www/js/script.js
+++ appcode/Byplastic/www/js/script.js
@@ -1,13 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCL5q-TjV95iEESOvOmf419Ovhim6HmMU0",
+        authDomain: "gjden-ac410.firebaseapp.com",
+        databaseURL: "https://gjden-ac410.firebaseio.com",
+        storageBucket: "gjden-ac410.appspot.com",
+        messagingSenderId: "951073665617"
+    };
+    firebase.initializeApp(config);
+
     
-    
-    
-    
-   console.log("test"); 
-    
+
+
+    console.log("test");
+
     var params = {
-  fields: 'id,note,link,image'
-};
-    
-    
+        fields: 'id,note,link,image'
+    };
+
+
+
 });
